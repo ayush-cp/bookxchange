f6044c576af2c6b78894416c77ed997826ca616e
wbidit <brb020@nist.edu>
2024-11-27 11:50:14 +0530

    updated profile page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,19 +3,30 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Signup from "./components/Signup/Signup";
 import BookSearch from "./components/BookSearch/BookSearch";
+import ProfilePage from "./components/ProfilePage/ProfilePage";
+// import ResponsiveProfilePage from "./components/ProfilePage/responsive-profile-page";
+// import ProfilePage from "./components/ProfilePage/ProfilePage
 
 function App() {
-  return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/bookSearch" element={<BookSearch />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    {/* Define route for Home */}
+                    <Route path="/" element={<Home />} />
+
+                    {/* Define route for Signup */}
+                    <Route path="/signup" element={<Signup />} />
+
+                    {/* Define route for BookSearch */}
+                    <Route path="/booksearch" element={<BookSearch />} />
+
+                    {/* Define route for Profile Page */}
+                    <Route path="/profile" element={<ProfilePage/>} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
