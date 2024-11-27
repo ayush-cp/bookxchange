d9204d2ba36524c550c95a3ede5f6c4b07c679fd
Ayush Panwar <ayushpanwarcp92@gmail.com>
2024-11-27 14:29:46 +0530

    Added onload animation on hero image.

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import hero from "/public/images/hero.png"
+import React, { useEffect, useRef, useState } from "react";
+import hero from "/public/images/hero.png";
 import Navbar from "../Layout/Navbar";
 import Review from "../Review/Review";
 import Goal from "../Goal/Goal";
@@ -9,47 +9,104 @@ import Footer from "../Layout/Footer";
 import { Link, Navigate, useNavigate } from "react-router-dom";
 
 const Home = () => {
-  const navigate=useNavigate();
+ 
+  const imageRef = [useRef(null), useRef(null), useRef(null)];
+  const [visibleItem, setVisibleItem] = useState(
+    new Array(imageRef.length).fill(false)
+  );
+  useEffect(() => {
+   
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.3,
+    }
+    const observer = new IntersectionObserver((entries) => {
+      const intersectingElements = [...visibleItem]
+      entries.forEach((entry, index)=>{
+        const elementIndex = imageRef.findIndex(ref=>ref.current===entry.target)
+        if(elementIndex !== -1 && entry.isIntersecting){
+          intersectingElements[elementIndex] = true
+          console.log(elementIndex, intersectingElements[elementIndex]);
+          observer.unobserve(entry.target)
+        }
+      })
+      setVisibleItem(intersectingElements)
+    }, options)
+
+    imageRef.forEach((ref)=>{
+      if (ref.current) {
+        observer.observe(ref.current)
+      }
+    })
+
+
+    return () => {
+      
+      imageRef.forEach((ref)=>{
+        if (ref.current) {
+          observer.unobserve(ref.current)
+        }
+      })
+    };
+  }, []);
+
+  // const navigate=useNavigate();
   return (
     <div className="relative w-full h-full flex flex-col gap-4">
-      <div className=" fixed w-full h-max z-10">
+      <div className="transition-all ease-linear duration-500 fixed w-full z-10 h-max">
         <Navbar />
       </div>
-      <div id="home" className="relative w-full md:h-[90vh] h-[80vh] bg-gradient-to-br from-gray-50 from-60% via-blue-50 to-cyan-50 to-50% flex justify-center items-center z-0 overflow-hidden">
-        <div className="md:w-[50%] w-[60%] md:h-[75%] h-[80%] absolute bottom-[5%] right-[-5%] z-[-1]">
-          <img src={hero} alt="hero image" className="w-full h-full object-contain"/>
+      <div
+        id="home"
+        className="relative w-full md:h-[90vh] h-[80vh] bg-gradient-to-br from-gray-50 from-60% via-blue-50 to-cyan-50 to-50% flex justify-center items-center z-0 overflow-hidden"
+      >
+        <div
+          ref={imageRef[0]}
+          className={`transition-all ease-linear duration-500 z-0 md:w-[50%] w-[60%] md:h-[75%] h-[80%] absolute bottom-[5%] ${
+            visibleItem[0] ? "right-[-5%] opacity-100" : "right-[-10%] opacity-0"
+          }`}
+        >
+          <img
+            src={hero}
+            alt="hero image"
+            className="w-full h-full object-contain"
+          />
         </div>
-        <div className=" md:w-1/2 w-full h-max flex flex-col items-center gap-4">
-          <h1 className="flex flex-col gap-4 md:text-5xl sm:text-3xl text-2xl font-semibold text-gray-800">
-            <span>Share Knowledge</span> <span className="text-emerald-700 ml-20">Increase Knowledge</span>
+        <div className="z-10 md:w-1/2 w-full h-max flex flex-col items-center gap-4">
+          <h1 ref={imageRef[1]} className={`${visibleItem[1]?'scale-100 opacity-100':'scale-60 opacity-0'} flex transition-all ease-linear duration-500 flex-col gap-4 md:text-5xl sm:text-3xl text-2xl font-semibold text-gray-800`}>
+            <span>Share Knowledge</span>{" "}
+            <span className="text-emerald-700 ml-20">Increase Knowledge</span>
           </h1>
           <div className="flex flex-row gap-4">
-          <button className="bg-blue-400 hover:bg-blue-500 transition-all ease-out duration-300 md:px-4 sm:px-2 px-2 py-1 rounded-lg md:text-2xl sm:text-xl text-md font-semibold text-gray-50">
-           <Link to="/bookSearch">New Read</Link> 
-          </button>
-          <button className="bg-green-400 hover:bg-green-500 transition-all ease-out duration-300 md:px-4 sm:px-2 px-2 py-1 rounded-lg md:text-2xl sm:text-xl text-md font-semibold text-gray-50">
-          <Link to="/profile">Share Book</Link> 
-          </button>
+            <button className="bg-blue-400 hover:bg-blue-500 transition-all ease-out duration-300 md:px-4 sm:px-2 px-2 py-1 rounded-lg md:text-2xl sm:text-xl text-md font-semibold text-gray-50">
+              <Link to="/bookSearch">New Read</Link>
+            </button>
+            <button className="bg-green-400 hover:bg-green-500 transition-all ease-out duration-300 md:px-4 sm:px-2 px-2 py-1 rounded-lg md:text-2xl sm:text-xl text-md font-semibold text-gray-50">
+              <Link to="/profile">Share Book</Link>
+            </button>
           </div>
-         
-          <h3 className="md:text-xl sm:text-lg text-md font-semibold text-black"><span className="text-green-500"> 10.5K+ </span>Books Exchanged</h3>
+
+          <h3 className="md:text-xl sm:text-lg text-md font-semibold text-black">
+            <span className="text-green-500"> 10.5K+ </span>Books Exchanged
+          </h3>
         </div>
       </div>
 
       <div id="review" className="w-full h-max ">
-        <Review/>
+        <Review />
       </div>
       <div className="w-full h-max ">
-        <Goal/>
+        <Goal />
       </div>
       <div className="w-full h-max">
-        <Recommend/>
+        <Recommend />
       </div>
       <div id="faqs" className="w-full h-max">
-        <Faqs/>
+        <Faqs />
       </div>
       <div className="w-full h-max">
-        <Footer/>
+        <Footer />
       </div>
     </div>
   );
--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -1,17 +1,24 @@
 import React, { useState } from "react";
 import ham from "/public/images/ham.png";
-import bookLogo from "/public/images/bookLogo.png"
+import bookLogo from "/public/images/bookLogo.png";
 import { Link } from "react-router-dom";
-
+import user from "/public/images/user.png";
 
 const Navbar = () => {
   const [menu, setMenu] = useState(false);
+  const [userLogged, setUserLogged] = useState(false);
   return (
     <div className="w-full h-max">
       <div className="relative w-full h-full p-4 bg-gray-300 bg-opacity-50 backdrop-blur-md flex flex-row justify-between px-8">
         <div className="w-20 h-11 cursor-pointer ">
-          <Link to="/"> <img src={bookLogo} alt="" className="w-full h-full object-contain"/></Link>
-         
+          <Link to="/">
+            {" "}
+            <img
+              src={bookLogo}
+              alt=""
+              className="w-full h-full object-contain"
+            />
+          </Link>
         </div>
         <div className=" md:w-[30%] w-full h-max flex md:flex-row md:px-0 px-6 justify-end items-center gap-8">
           <div
@@ -28,20 +35,27 @@ const Navbar = () => {
             } md:flex md:flex-row md:bg-transparent bg-gray-200 flex-col md:static absolute top-[100%] right-0 justify-between items-center md:gap-0 gap-3`}
           >
             <li className="text-gray-800 relative md:w-max w-full text-center  cursor-pointer font-sans text-lg font-semibold transition-all ease-linear duration-200 p-2 py-1 hover:bg-[#e8e7e7ca] rounded-[0.4rem] after:transition-all after:duration-300 hover:drop-shadow-[0px_1px_1px_#c8f2fd] after:ease-in after:content-[''] after:h-[2px] hover:after:w-full after:w-0 after:absolute after:bg-black after:bottom-0 after:left-0">
-             <a href="#home">Home</a> 
+              <a href="#home">Home</a>
             </li>
             <li className="text-gray-800 relative md:w-max w-full text-center  cursor-pointer font-sans text-lg font-semibold transition-all ease-linear duration-200 p-2 py-1 hover:bg-[#e8e7e7ca] rounded-[0.4rem] after:transition-all after:duration-300 hover:drop-shadow-[0px_1px_1px_#c8f2fd] after:ease-in after:content-[''] after:h-[2px] hover:after:w-full after:w-0 after:absolute after:bg-black after:bottom-0 after:left-0">
-            <a href="#review">Review</a> 
+              <a href="#review">Review</a>
             </li>
             <li className="text-gray-800 relative md:w-max w-full text-center  cursor-pointer font-sans text-lg font-semibold transition-all ease-linear duration-200 p-2 py-1 hover:bg-[#e8e7e7ca] rounded-[0.4rem] after:transition-all after:duration-300 hover:drop-shadow-[0px_1px_1px_#c8f2fd] after:ease-in after:content-[''] after:h-[2px] hover:after:w-full after:w-0 after:absolute after:bg-black after:bottom-0 after:left-0">
-            <a href="#faqs">Faqs</a> 
+              <a href="#faqs">Faqs</a>
             </li>
-            
           </ul>
           <div>
-            <button className="bg-blue-500 transition-all ease-linear duration-200 hover:bg-[#4a42e6c4] px-4 py-1 rounded-md text-white text-lg font-semibold font-sans">
-             <Link to="/login">Login</Link> 
-            </button>
+            {userLogged ? (
+              <button className="bg-blue-500 transition-all ease-linear duration-200 hover:bg-[#4a42e6c4] px-4 py-1 rounded-md text-white text-lg font-semibold font-sans">
+                <Link to="/profile">
+                  <img src={user} alt="" className="w-10 "/>
+                </Link>
+              </button>
+            ) : (
+              <button className="bg-blue-500 transition-all ease-linear duration-200 hover:bg-[#4a42e6c4] px-4 py-1 rounded-md text-white text-lg font-semibold font-sans">
+                <Link to="/login">Login</Link>
+              </button>
+            )}
           </div>
         </div>
       </div>
